a80abbff5d90d99dd2ffbe8ca344808f387c9f09
Ciaran Maye <58376132+monc949@users.noreply.github.com>
2021-09-17 13:44:24 +0100

    Delete Globals.java

--- src/Controller/Globals.java
+++
@@ -1,8 +0,0 @@
-package Controller;
-public interface Globals {
-
-    public static final String DATABASE_URL = "jdbc:mysql://localhost/leaguetracker";
-    public static final String USER = "root";
-    public static final String PASSWORD = "Knockbeg112";
-
-}
