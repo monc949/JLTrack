1f84fff9657b0adcfc1350d74725e2fe19267742
Ciaran Maye <ciaranmaye8@gmail.com>
2021-09-17 13:43:29 +0100

    .

---
+++ src/Controller/GlobalsExample.java
@@ -0,0 +1,9 @@
+package Controller;
+public interface GlobalsExample {
+
+    // Your details here
+    public static final String DATABASE_URL = "jdbc:mysql://localhost/leaguetracker";
+    public static final String USER = "root";
+    public static final String PASSWORD = "PASSWORD";
+
+}
