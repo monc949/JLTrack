1ba2e5e6abf51e735aab3bd923142e0b750ccf50
Ciaran <ciaranmaye8@gmail.com>
2021-11-25 11:36:59 +0000

    .

--- src/Controller/Globals.java
+++ src/Controller/Globals.java
@@ -4,7 +4,7 @@ public interface Globals {
 
     // Your details here
     public static final String DATABASE_URL = "jdbc:mysql://localhost/leagueTracker2";
-    public static final String USER = "tracker";
-    public static final String PASSWORD = "trackerpass";
+    public static final String USER = "leagueAdmin";
+    public static final String PASSWORD = "trackerPass";
 
 }
