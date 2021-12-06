b4c1f2bf2e227739766d42222af95252019d6d2c
Ciaran Maye <ciaranmaye8@gmail.com>
2021-12-06 22:28:43 +0000

    global change

--- src/Controller/Globals.java
+++ src/Controller/Globals.java
@@ -3,7 +3,7 @@ package Controller;
 public interface Globals {
 
     // Your details here
-    public static final String DATABASE_URL = "jdbc:mysql://redtosh.ddns.net/leagueTracker";
+    public static final String DATABASE_URL = "jdbc:mariadb://redtosh.ddns.net/leagueTracker";
     public static final String USER = "leagueAdmin";
     public static final String PASSWORD = "trackerPass";
 
